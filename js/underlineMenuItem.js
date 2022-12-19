e4317a488226efb49ffe4ed1f533aed9df4fea89
t1mtg <timagurman@gmail.com>
2022-12-19 13:03:33 +0300

    fix: lab4 requested changes

--- js/underlineMenuItem.js
+++ js/underlineMenuItem.js
@@ -1,15 +1,11 @@
 const navbarItems = document.querySelectorAll(".header-content-pages__link");
 const splittedData = document.location.href.split("/");
 const currentLocation = splittedData[splittedData.length - 1].split(".")[0];
-const matchLocation = {
-    'посылки': 'index',
-    'работники': 'workers',
-    'помещения': 'premises',
-};
 
 navbarItems.forEach((item) => {
+
     if (
-        matchLocation[item.textContent.toLowerCase()] === currentLocation
+        item.querySelector('a').href.split('/').slice(-1)[0].split('.')[0] === currentLocation
     ) {
         navbarItems.forEach((item) =>
             item.classList.remove("header-content-pages__link--active")
